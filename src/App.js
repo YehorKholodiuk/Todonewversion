23b508ca2dff2612619f608c54200af248370e9c
ASUS <Yehor Kholodiuk>
2021-12-03 16:43:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {useState} from "react";
 function App() {
     const [task, setTask] = useState('Alice')
     const [tasks, setTasks] = useState([{id: 1, title: 'first task', done:false}])
+    const [priority, setPriority] = useState(5);
+
     const inputOnChange = (e) => {
         console.log(e.target.value)
         setTask(e.target.value)
@@ -21,6 +23,21 @@ function App() {
     }:el);
     setTasks(updatedTasks)
     }
+
+
+    const movePosition = (index, newIndex) => {
+        const newTasks = [...tasks];
+        const temp = newTasks[index];
+        newTasks[index] = newTasks[newIndex];
+        newTasks[newIndex] = temp;
+        setTasks(newTasks)
+    }
+
+    const deleteTask =(id) => {
+        const newList = tasks.filter(el => el.id !== id)
+        setTasks (newList)
+    }
+
     return (
         <div>
             <h1>{task}</h1>
@@ -28,9 +45,18 @@ function App() {
             <button onClick={addTask}>Add Task</button>
             <ul>
                 {
-                    tasks.filter(el => !el.done).map(el => <li key={el.id} className={el.done?'done':' '}>
+                    tasks.filter(el => !el.done).map((el,i) => <li key={el.id} className={el.done?'done':' '}>
                         {el.title}{el.done?'completed':'uncompleted'}
-                        <button onClick={() => onDoneToggle(el.id)}>{el.done ? 'Mark as uncompleted':'Mark as completed'}</button></li>)
+                        <button onClick={() => onDoneToggle(el.id)}>{el.done ? 'Mark as uncompleted':'Mark as completed'}</button>
+
+                        { i !== 0 && <button onClick={() => movePosition(i, i - 1)}> ↑ </button>}
+                        { i !== tasks.length - 1 && <button onClick={() => movePosition(i, i + 1)}> ↓ </button>}
+
+
+
+
+
+                    </li>)
                 }
             </ul>
 <hr/>
@@ -39,7 +65,10 @@ function App() {
                 {
                     tasks.filter(el => el.done).map(el => <li key={el.id} className={el.done?'done':' '}>
                         {el.title}{el.done?'completed':'uncompleted'}
-                        <button onClick={() => onDoneToggle(el.id)}>{el.done ? 'Mark as uncompleted':'Mark as completed'}</button></li>)
+                        <button onClick={() => onDoneToggle(el.id)}>{el.done ? 'Mark as uncompleted':'Mark as completed'}</button>
+                    <button onClick={() => deleteTask(el.id)}>Delete </button>
+                    </li>)
+
                 }
             </ul>
 
